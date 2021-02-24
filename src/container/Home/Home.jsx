e0ac32929f9b02a858b173b9c8f125a29c264fb1
RianMupti <rianmupti@gmail.com>
2021-02-24 09:11:28 +0700

    mengupdate parent oleh child secara dinamis

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,17 +1,22 @@
 import React, { Fragment } from 'react';
 import YoutubeComp from '../../component/YoutubeComp/YoutubeComp';
+import Product from '../Product/Product';
 
 class Home extends React.Component {
     render() {
         return (
             <Fragment>
-                <h3>Youtube Component</h3>
+                {/* <h3>Youtube Component</h3>
                 <hr />
                 <YoutubeComp time="7.12" title="Contoh 1" desc="Lorem ipsum dolor sit amet consectetur adipisicing." />
                 <YoutubeComp time="8.12" title="Contoh 2" desc="Lorem ipsum dolor sit amet consectetur adipisicing." />
                 <YoutubeComp time="12.12" title="Contoh 3" desc="Lorem ipsum dolor sit amet consectetur adipisicing." />
                 <YoutubeComp time="9.12" title="Contoh 4" desc="Lorem ipsum dolor sit amet consectetur adipisicing." />
-                <YoutubeComp />
+                <YoutubeComp /> */}
+
+                <p>Product</p>
+                <hr />
+                <Product />
             </Fragment>
         )
     }
---
+++ src/container/Product/Product.jsx
@@ -0,0 +1,35 @@
+import React, { Component, Fragment } from 'react';
+import CardProduct from '../CardProduct/CardProduct';
+import './Product.css';
+
+class Product extends Component {
+    state = {
+        order: 3,
+        name: "Rian Mupti Jauhari"
+    }
+
+    ketikaberubah = (newValue) => {
+        this.setState({
+            order: newValue
+        })
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <div className="header">
+                    <div className="logo">
+                        <img src="" alt="logo" />
+                    </div>
+                    <div className="troley">
+                        <img src="" alt="troley" />
+                        <div className="count">{this.state.order}</div>
+                    </div>
+                </div>
+                <CardProduct onChange={(Value) => this.ketikaberubah(Value)} />
+            </Fragment>
+        )
+    }
+}
+
+export default Product;
