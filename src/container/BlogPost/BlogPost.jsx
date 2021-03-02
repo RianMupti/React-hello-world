dd6c296df0506caebd55830f014bc1dec378fb4b
RianMupti <rianmupti@gmail.com>
2021-03-02 18:09:58 +0700

    melanjutkan belajar api get bagian 2

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,13 +1,44 @@
 import React, { Component, Fragment } from 'react';
 import Post from '../../component/Post/Post';
 import './BlogPost.css';
+import axios from 'axios';
 
 class BlogPost extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            post: []
+        }
+    }
+
+    componentDidMount() {
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        //     .then(response => response.json())
+        //     .then(json => {
+        //         this.setState({
+        //             post: json
+        //         })
+        //     })
+
+        // Axios
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((result) => {
+                this.setState({
+                    post: result.data
+                })
+            })
+    }
+
     render() {
         return (
             <Fragment>
                 <p className="section-title">Blog Post</p>
-                <Post title="title" desc="desc" />
+                {
+                    this.state.post.map((res) => {
+                        return <Post key={res.id} title={res.title} desc={res.body} />
+                    })
+                }
+
             </Fragment>
         )
     }
