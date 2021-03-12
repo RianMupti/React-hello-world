9b64c9570b754b4e7c3e5af431a66e2bd87481fa
RianMupti <rianmupti@gmail.com>
2021-03-12 11:17:37 +0700

    melanjutkan routing react routing dom

--- src/container/Product/Product.jsx
+++ src/container/Product/Product.jsx
@@ -17,6 +17,8 @@ class Product extends Component {
     render() {
         return (
             <Fragment>
+                <p>Product</p>
+                <hr />
                 <div className="header">
                     <div className="logo">
                         <img src="" alt="logo" />
