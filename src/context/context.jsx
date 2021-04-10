3e1835b59cd7f27b3c8b7d52735a45dc7ec6dd27
RianMupti <rianmupti@gmail.com>
2021-04-10 09:48:15 +0700

    Refactoring Context API dengan HOC part4

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -6,32 +6,32 @@ const RootContext = createContext();
 const Provider = RootContext.Provider;
 
 const GlobalProvider = (Children) => {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            totalOrder: 3
-        }
-    }
-
-    dispatch = (action) => {
-        if (action.type === 'PLUS_ORDER') {
-            this.setState({
-                totalOrder: this.state.totalOrder + 1
-            })
-        }
-
-        if (action.type === 'MINUS_ORDER') {
-            if (this.state.totalOrder > 0) {
-                this.setState({
-                    totalOrder: this.state.totalOrder - 1
-                })
-            }
-        }
-    }
-
     return (
+
         class ParentComp extends Component {
+            constructor(props) {
+                super(props)
+                this.state = {
+                    totalOrder: 3
+                }
+            }
+
+            dispatch = (action) => {
+                if (action.type === 'PLUS_ORDER') {
+                    this.setState({
+                        totalOrder: this.state.totalOrder + 1
+                    })
+                }
+
+                if (action.type === 'MINUS_ORDER') {
+                    if (this.state.totalOrder > 0) {
+                        this.setState({
+                            totalOrder: this.state.totalOrder - 1
+                        })
+                    }
+                }
+            }
+
             render() {
                 return (
                     <Provider value={
@@ -52,3 +52,23 @@ const GlobalProvider = (Children) => {
 export default GlobalProvider;
 
 // Consumer
+const Consumer = RootContext.Consumer;
+export const GlobalConsumer = (Children) => {
+    return (
+        class ParentConsumer extends Component {
+            render() {
+                return (
+                    <Consumer>
+                        {
+                            (value) => {
+                                return (
+                                    <Children {...this.props} {...value} />
+                                )
+                            }
+                        }
+                    </Consumer>
+                )
+            }
+        }
+    )
+}
