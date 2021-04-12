2d506a08b71bb5856a81bde31314f97880bd23c5
RianMupti <rianmupti@gmail.com>
2021-04-13 05:49:54 +0700

    membuat Global API Service (GET) part 1 oke

---
+++ src/service/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const RootPath = 'http://localhost:3004';
+const OnlineRoot = 'https://jsonplaceholder.typicode.com'
+
+const Get = (path, root) => () => {
+    const promise = new Promise((resolve, reject) => {
+        axios.get(`${root ? OnlineRoot : RootPath}/${path}`) //Operator ternari
+            .then((result) => {
+                resolve(result.data);
+            }, (e) => {
+                reject(e)
+            })
+    })
+    return promise;
+}
+
+const getNewsBlog = Get('posts?_sort=id&_order=desc', false);
+const getComment = Get('comments', true);
+
+const API = {
+    getNewsBlog,
+    getComment
+}
+
+export default API;
+
